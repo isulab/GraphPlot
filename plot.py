bdb720613ab73fdcd65da9ca01a03094a07fa4b5
tommy19970714 <toshiki0714@icloud.com>
2017-05-12 10:47:05 +0900

    read header to plot.py

--- plot.py
+++ plot.py
@@ -15,31 +15,37 @@ if not args.filename:
     ##args.filename = ["5Hz.csv"] ##test用
     exit()
 
-def loadText(filename,column):
+def loadText(filename,columnName,columnDefaluts):
     f = open(filename,"r")
+    readColumn = columnDefaluts
     result = []
+
+    header = f.readline()
+    for i,head in enumerate(header.split(',')):
+        if head == columnName:
+            readColumn = i
 
     lines = f.readlines()
     for line in lines:
-        data = float(line.split(',')[column])
+        data = float(line.split(',')[readColumn])
         result.append(data)
 
     f.close()
     return result
 
 def draw(filename):
-    x = loadText(filename,0)
-    y1 = loadText(filename,1)
+    x = loadText(filename, columnName="time[s]", columnDefaluts=0)
+    y1 = loadText(filename, "send1", 1)
     plt.plot(x,y1)
-    y2 = loadText(filename,2)
+    y2 = loadText(filename, "send2", 2)
     plt.plot(x,y2)
-    y3 = loadText(filename,3)
+    y3 = loadText(filename, "send3", 3)
     plt.plot(x,y3)
-    y4 = loadText(filename,4)
+    y4 = loadText(filename, "recieve1", 4)
     plt.plot(x,y4)
-    y5 = loadText(filename,5)
+    y5 = loadText(filename, "recieve2", 5)
     plt.plot(x,y5)
-    y6 = loadText(filename,6)
+    y6 = loadText(filename, "recieve3", 6)
     plt.plot(x,y6)
 
 def main():
